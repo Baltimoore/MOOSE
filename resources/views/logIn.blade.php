fca54dc82914b9aa1eaf58f579a059d18998d65a
Gatis Ieviņš <gi17001@students.lu.lv>
2021-12-06 19:22:21 +0200

    pieslēgšanās atjaununājums

--- resources/views/logIn.blade.php
+++ resources/views/logIn.blade.php
@@ -22,16 +22,16 @@ Reģistrācija
     <div class="w3-container w3-center">
         <img style="height:40vh;max-height:700px;max-width:700px;"
              src="/images/logo.svg" alt="Medību biedrības logo">
-        <h1 style="margin-bottom:5vh"><b>Mednieku biedrība "Alnis"</b></h1>
+        <h1 style="margin-bottom: 5vh"><b>Mednieku biedrība "Alnis"</b></h1>
 
-        <div style="width:auto;max-width:400px;margin:auto">
+        <div style="width:auto; max-width:400px; margin:auto">
             <label for="uname"><b>Lietotājvārds</b></label>
-            <input class="w3-input" type="text" placeholder="ex: ei26745" name="uname" required>
+            <input class="w3-input w3-round-large" type="text" placeholder="ex: ei26745" name="uname" required>
             <br>
             <label for="psw"><b>Parole</b></label>
-            <input class="w3-input" type="password" placeholder="Ievadiet paroli" name="psw" required>
+            <input class="w3-input w3-round-large" type="password" placeholder="Ievadiet paroli" name="psw" required>
             <br>
-            <a class="w3-button" style="background-color:var(--logo-fur-light);" type="submit" href="{{ route('home') }}"><b>Pieslēgties</b></a>
+            <a class="w3-button w3-round-large" href="{{ route('home') }}"><b>Pieslēgties</b></a>
             <br>
             <label><input class="w3-check" type="checkbox" checked="checked"> Saglabāt lietotājvārdu</label>
         </div>
