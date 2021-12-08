a037c67962745959af2d875be2556c0b6956b833
Gatis Ieviņš <gi17001@students.lu.lv>
2021-12-08 14:01:36 +0200

    pievienoju teritorijas notikumu sarakstu

--- resources/views/registered/map.blade.php
+++ resources/views/registered/map.blade.php
@@ -8,6 +8,7 @@
  - - - - - - - - - - - --}}
 
 @section('moduleStyle')
+/* pagaidu iemestā attēla formāts*/
 #mapInteractive {
     background-color: var(--logo-fur-dark);
     border: 2px solid var(--logo-outline);
@@ -19,6 +20,7 @@
         width: 100%;
     }
 
+/* atlasītās teritorijas info gabals */
 #mapInfo {
     background-color: var(--logo-bone-light);
     border:2px solid var(--logo-outline);
@@ -37,6 +39,44 @@
     #mapInfo > div > * { display: inline-block; }
     #mapInfo p { line-height:0; }
     #mapInfo > div > label { font-weight: bold; }
+
+/* piešķir medību notikumu sarakstam virsrakstu */
+#areaEvents::before {
+    font-family: "Montserrat Alternates", sans-serif;
+    position: absolute;
+    margin-top: -13px;
+    padding: 0 5px;
+    margin-left: 20px;
+    background: #fff;
+    content:'Tuvākie teritorijas medību notikumi';
+}
+/* medību notikumu saraksta saturs */
+#areaEvents {
+    border: 2px solid var(--logo-outline);
+    border-radius: 25px;
+    margin: 30px 0;
+    padding: 0 10px;
+}
+    #areaEvents > div {
+        padding: 10px 0;
+        display: grid;
+        grid-template-columns: 130px 150px 250px 100px;
+        align-items: center;
+        text-align: center;
+        border-bottom: 1px dashed var(--logo-fur-light);
+    }
+    #areaEvents > div:last-child { border: 0; }
+    #areaEvents > #eventLegend {
+        font-weight: bold;
+        border-bottom: 2px dashed var(--logo-fur-dark);
+        padding-bottom: 0;
+    }
+    .eventDescription {
+        grid-column: 1 / -1;
+        line-height: 1;
+    }
+
+
 @endsection
 
 @section('moduleTitle')
@@ -54,6 +94,28 @@ medību teritorijas
         <div id="mapLicense"><label>Medību atļauja sniegta:</label> <a id="licenseOwner">Medību biedrība "Alnis"</a></div>
         <div id="mapLicenseDate"><label>Medību atļaujas termiņš:</label> <p id="licenseDate">2018-2023</p></div>
         <p>papildus teritorijas informācija</p>
+    </div>
+    <div id="areaEvents">
+        <div id="eventLegend">
+            <p>Datums</p>
+            <p>Medību tips</p>
+            <p>Medību vadītājs</p>
+            <p>Pieteikties medībām</p>
+        </div>
+        <div id="evnt1" class="eventInfo">
+            <p>14. maijs</p>
+            <b>Dzinējmedības</b>
+            <a href="#">Alnis Mūsis</a>
+            <a class="w3-button w3-round-large" href="#">Pieteikties</a>
+            <p class="eventDescription">Sapulcējamies pie nolauztās pastkastes</p>
+        </div>
+        <div id="evnt2" class="eventInfo">
+            <p>21. maijs</p>
+            <b>Medības</b>
+            <a href="#">Juris Padels</a>
+            <a class="w3-button w3-round-large" href="#">Pieteikties</a>
+            <p class="eventDescription">Sapulcējamies pie 3. izcirstās gravas no Jaunmuižas ceļa</p>
+        </div>
     </div>
 @endsection
 
