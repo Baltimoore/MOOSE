544c4e37f2bc484c28a118d0a632355c97dc5a99
Gatis Ieviņš <gi17001@students.lu.lv>
2021-12-15 16:46:07 +0200

    ilgi gaidīts, bet beidzot aizpildīts ar datiem

--- resources/views/registered/recordings.blade.php
+++ resources/views/registered/recordings.blade.php
@@ -17,7 +17,7 @@
     .vidArea > div {
         padding: 10px 0;
         display: grid;
-        grid-template-columns: 100px 300px auto 100px;
+        grid-template-columns: 80px 150px auto 100px;
         align-items: center;
         text-align: center;
         border-bottom: 1px dashed var(--logo-fur-light);
@@ -27,31 +27,15 @@
 .videoThumb {  }
     .videoThumb > img {
         object-fit: cover;
-        width: 40px;
+        width: 70px;
         height: 40px;
         background-color: var(--logo-fur-light);
-        border-radius: 50%;
     }
 .videoName {
     font-weight: bold;
 }
 
 .empty { display: none!important; }
-
-/* Piešķir visiem vietu konteineriem virsrakstus */
-.vidArea::before {
-    font-family: "Montserrat Alternates", sans-serif;
-    position: absolute;
-    margin-top: -13px;
-    padding: 0 5px;
-    margin-left: 20px;
-    background: #fff;
-}
-    #userHead::before { content: 'Biedrības vadītāji'; }
-    #userHand::before { content: 'Medību vadītāji'; }
-    #userGun::before { content: 'Mednieki'; }
-    #userRun::before { content: 'Dzinēji'; }
-    #userOther::before { content: 'Cits'; }
 
 @endsection
 
@@ -65,87 +49,35 @@ Lietotāju saraksts
     <div id="userHead" class="vidArea">
         <div id="vid1">
             <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Alnis Mūsis</a>
-            <p class="videoDescription"></p>
+            <a class="videoName" href="#">2021-05-23</a>
+            <p class="videoDescription">baigi lielais video apraksts ar lieliem un svarīgiem vārdiem, teksts aizņems baigi daudz vietas - veselus 500 simbolus, ja no galvas pareizi atminos. tā kā jāaizņem ļoti daudz vietas.</p>
             <div class="edit w3-button w3-round-large">Labot</div>
         </div>
-    </div>
-
-    <div id="userHand" class="vidArea">
         <div id="vid2">
             <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Juris Padels</a>
-            <p class="videoDescription"></p>
+            <a class="videoName" href="#">2021-05-16</a>
+            <p class="videoDescription">baigi lielais video apraksts ar lieliem un svarīgiem vārdiem, teksts aizņems baigi daudz vietas - veselus 500 simbolus, ja no galvas pareizi atminos. tā kā jāaizņem ļoti daudz vietas.</p>
             <div class="edit w3-button w3-round-large">Labot</div>
         </div>
         <div id="vid3">
             <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Miķelis Zindars</a>
-            <p class="videoDescription"></p>
+            <a class="videoName" href="#">2021-04-30</a>
+            <p class="videoDescription">baigi lielais video apraksts ar lieliem un svarīgiem vārdiem, teksts aizņems baigi daudz vietas - veselus 500 simbolus, ja no galvas pareizi atminos. tā kā jāaizņem ļoti daudz vietas.</p>
             <div class="edit w3-button w3-round-large">Labot</div>
         </div>
-    </div>
-    
-    <div id="userGun" class="vidArea">
         <div id="vid4">
             <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Dāvids Lurjevs</a>
-            <p class="videoDescription"></p>
+            <a class="videoName" href="#">2021-04-08</a>
+            <p class="videoDescription">baigi lielais video apraksts ar lieliem un svarīgiem vārdiem, teksts aizņems baigi daudz vietas - veselus 500 simbolus, ja no galvas pareizi atminos. tā kā jāaizņem ļoti daudz vietas.</p>
             <div class="edit w3-button w3-round-large">Labot</div>
         </div>
         <div id="vid5">
             <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Storks Stārķis</a>
-            <p class="videoDescription"></p>
-            <div class="edit w3-button w3-round-large">Labot</div>
-        </div>
-        <div id="vid6">
-            <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Bēre Lāce</a>
-            <p class="videoDescription"></p>
-            <div class="edit w3-button w3-round-large">Labot</div>
-        </div>
-        <div id="vid7">
-            <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Vofs Vilks</a>
-            <p class="videoDescription"></p>
-            <div class="edit w3-button w3-round-large">Labot</div>
-        </div>
-        <div id="vid8">
-            <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Foksis Lapsa</a>
-            <p class="videoDescription"></p>
-            <div class="edit w3-button w3-round-large">Labot</div>
-        </div>
-        <div id="vid9">
-            <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Bevers Bebris</a>
-            <p class="videoDescription"></p>
+            <a class="videoName" href="#">2021-03-25</a>
+            <p class="videoDescription">baigi lielais video apraksts ar lieliem un svarīgiem vārdiem, teksts aizņems baigi daudz vietas - veselus 500 simbolus, ja no galvas pareizi atminos. tā kā jāaizņem ļoti daudz vietas.</p>
             <div class="edit w3-button w3-round-large">Labot</div>
         </div>
     </div>
-
-    <div id="userRun" class="vidArea">
-        <div id="vid10">
-            <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Labris Dors</a>
-            <p class="videoDescription"></p>
-            <div class="edit w3-button w3-round-large">Labot</div>
-        </div>
-        <div id="vid11">
-            <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Veins Mārietis</a>
-            <p class="videoDescription"></p>
-            <div class="edit w3-button w3-round-large">Labot</div>
-        </div>
-        <div id="vid12" class="empty">
-            <div class="videoThumb"><img src="/images/logo.svg" alt="Lietotāja profila attēls"></div>
-            <a class="videoName" href="#">Hotto Taksis</a>
-            <p class="videoDescription"></p>
-            <div class="edit w3-button w3-round-large">Labot</div>
-        </div>
-    </div>
-
 </div>
 @endsection
 
